79156f0cb73aad45f5e2f8fd73d2068452dce87e
Paulo <paulo.hgf1408@gmail.com>
2021-08-23 23:31:55 -0300

    CHG: Finalizado modal de criação de conta ADD: Verificação de sessão nas rotas

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,32 @@
   <router-view/>
 </template>
 <script>
+import { watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import ModalFactory from './components/ModalFactory'
+import services from './services'
+import { setCurrentUser } from './store/user'
 
 export default {
   components: {
     ModalFactory
+  },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        const { data } = await services.user.getMe()
+        setCurrentUser(data)
+      }
+    })
   }
 }
 </script>
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
+import router from '../router'
 import AuthService from './auth'
+import UserService from './user'
+import { setGlobalLoading } from '../store/global'
 
 const API_ENVS = {
   production: '',
@@ -11,16 +14,36 @@ const httpClient = axios.create({
   baseURL: API_ENVS.local
 })
 
-httpClient.interceptors.response.use((response) => response, (error) => {
+httpClient.interceptors.request.use(config => {
+  setGlobalLoading(true)
+  const token = window.localStorage.getItem('token')
+
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+httpClient.interceptors.response.use((response) => {
+  setGlobalLoading(false)
+  return response
+}, (error) => {
   const canThrowAnError = [0, 500].includes(error.request.status)
+  setGlobalLoading(false)
 
   if (canThrowAnError) {
     throw new Error(error.message)
+  }
+
+  if (error.response.status === 401) {
+    router.push({ name: 'Home' })
   }
 
   return error
 })
 
 export default {
-  auth: AuthService(httpClient)
+  auth: AuthService(httpClient),
+  user: UserService(httpClient)
 }
